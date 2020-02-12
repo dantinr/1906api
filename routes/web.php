39330965bc0e2fe8a2b3251a1a74da865d7b000d
dante <dantinr@gmail.com>
2020-02-12 10:01:50 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,26 @@ Route::get('/', function () {
 Route::get('/phpinfo',function(){
     phpinfo();
 });
+
+
+###############  测试 路由 #########################
+
+
+Route::prefix('/test')->group(function(){
+    Route::get('/redis','TestController@testRedis');
+    Route::get('/test002','TestController@test002');
+    Route::get('/test003','TestController@test003');
+
+});
+
+###############  测试 路由 #########################
+
+
+
+Route::prefix('/api')->group(function(){
+
+});
+
+
+
+
